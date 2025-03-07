7b9fe5d57fc61fadaf89e2321e0cc4a7ad4dbcf0
Marek Kustosz <markust71@hotmail.com>
2025-03-07 15:31:02 +0100

    add WebSocket store and hook for MyApp integration; update API URL

---
+++ stores/my-app-websocket-store/my-app-websocket.store.types.ts
@@ -0,0 +1,10 @@
+export type MyAppWebSocketState = {
+  socket: WebSocket | null // Obiekt WebSocket
+  messages: string[] // Lista wiadomości
+  isConnected: boolean // Status połączenia
+  addMessage: (message: string) => void // Dodawanie wiadomości
+  setConnected: (status: boolean) => void // Ustawienie statusu połączenia
+  setSocket: (socket: WebSocket | null) => void // Ustawienie instancji WebSocket
+  sendMessage: (message: string) => void // Funkcja do wysyłania wiadomości
+  clearMessages: () => void // Czyszczenie wiadomości
+}
