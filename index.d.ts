64244c9b12cc2b82defe7f4ec6c80a553b51928e
Marek Kustosz <markust71@hotmail.com>
2025-03-16 23:47:10 +0100

    feat: enhance trade data handling with additional take profit fields and update date formatting

--- index.d.ts
+++ index.d.ts
@@ -1,10 +1,2 @@
 declare module '@binance/futures-connector'
 declare module '@binance/connector'
-
-import '@tanstack/react-table'
-
-declare module '@tanstack/react-table' {
-  interface ColumnMeta<TData, TValue> {
-    className?: string
-  }
-}
