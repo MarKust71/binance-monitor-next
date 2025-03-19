cd943c8b7c15eb5a7ffebd6e78a881c4e691def2
Marek Kustosz <markust71@hotmail.com>
2025-03-19 22:45:36 +0100

    feat: enhance DbTrades table columns with improved cell rendering and add number color utility

--- utils/format-number/format-number.ts
+++ utils/format-number/format-number.ts
@@ -1,7 +1,12 @@
-export const formatNumber = (value: number, fraction: number = 2): string =>
+export const formatNumber = (
+  value: number,
+  fraction: number = 2,
+  sign: boolean = false
+): string =>
   new Intl.NumberFormat('pl-PL', {
     style: 'decimal',
     minimumFractionDigits: fraction,
     maximumFractionDigits: fraction,
     useGrouping: true,
+    signDisplay: sign ? 'exceptZero' : 'negative',
   }).format(value)
